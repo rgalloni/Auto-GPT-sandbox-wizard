9febfd59dd782e0b3af1a76516838c0cb85a892f
Ruggero <ruggero.galloni@gmail.com>
2023-04-22 20:53:19 +0200

    Updated .ps1 with the same logic of .sh

--- auto_gpt_easy_install.ps1
+++ auto_gpt_easy_install.ps1
@@ -1,121 +1,124 @@
+Write-Host "Welcome to the Auto-GPT setup script! This script will guide you through the installation and configuration process."
+
 # Install Chocolatey if not already installed
 if (-not (Get-Command "choco" -ErrorAction SilentlyContinue)) {
     Set-ExecutionPolicy Bypass -Scope Process -Force
     iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
 
+$softwareInstalled = $false
+
 # Check if Docker is installed, otherwise install it
 if (-not (Get-Command "docker" -ErrorAction SilentlyContinue)) {
     Write-Host "Docker not found. Installing Docker..."
     choco install docker-desktop -y
-    Write-Host "Please restart the script after Docker has been installed and started."
-    exit
+    Write-Host "Updating Linux sub-system for Docker..."
+    $softwareInstalled = $true
 }
 
 # Check if git is installed, otherwise install it
 if (-not (Get-Command "git" -ErrorAction SilentlyContinue)) {
     Write-Host "Git not found. Installing Git..."
     choco install git -y
+    $softwareInstalled = $true
 }
 
-# Check if the Docker container exists
-$container_exists = docker ps -a --filter "name=auto-gpt-container" --format "{{.Names}}"
+if ($softwareInstalled) {
+    Write-Host "Software installed! Please restart your computer and run this script again."
+    exit
+}
 
-# Run Docker container if it doesn't exist, or start the existing container
-if (-not $container_exists) {
+# Updating WSL
+wsl --update
 
-    # Remove the cloned git repository from the local machine
-    Remove-Item -Recurse -Force "cloned_repo"
+# Check if Docker Desktop is running
+$docker = docker ps 2>&1
 
-    # Prompt user for API keys
-    $OPENAI_API_KEY = Read-Host "Enter your OpenAI API Key: "
-    $PINECONE_API_KEY = Read-Host "Enter your Pinecone API Key (long term memory: get for free at https://pinecone.io): "
-    $PINECONE_API_ENV = Read-Host "Enter your Pinecone API Env (long term memory: get for free at https://pinecone.io): "
+Write-Host "Please make sure Docker Desktop is running. If it's not, start Docker Desktop before proceeding."
+Read-Host "Press ENTER when Docker Desktop is running"
 
-    # Prompt user for workspace folder path
-    $default_workspace_path = Join-Path $env:USERPROFILE "auto_gpt_workspace"
-    $workspace_path = Read-Host "Enter the path of the workspace folder [press Enter for the default path: $($default_workspace_path)]"
-    if (-not $workspace_path) {
-        $workspace_path = $default_workspace_path
-    }
-    New-Item -ItemType Directory -Force -Path $workspace_path
+# Check if Auto-GPT folder exists and if it has a .git directory
+if (Test-Path "Auto-GPT") -and (Test-Path "Auto-GPT/.git") {
+    Set-Location Auto-GPT
+    # Fetch the latest changes from the remote repository
+    git fetch
 
-    # Check if git is installed, otherwise install it
-    if (-not (Get-Command "git" -ErrorAction SilentlyContinue)) {
-        if (Get-Command "apt-get" -ErrorAction SilentlyContinue) {
-            sudo apt-get update
-            sudo apt-get install -y git
-        } elseif (Get-Command "brew" -ErrorAction SilentlyContinue) {
-            brew install git
-        } else {
-            Write-Host "Error: Unable to find a suitable package manager to install git"
-            exit 1
+    # Check if there are updates available
+    if (git status -uno | Select-String "Your branch is behind") {
+        $update_choice = Read-Host "Updates are available. Would you like to update Auto-GPT to the latest version? (y/n): "
+        if ($update_choice -eq "y") {
+            $pre_pull_env_template = Get-Content .env.template
+            git pull
+            $post_pull_env_template = Get-Content .env.template
+            
+            if ($pre_pull_env_template -ne $post_pull_env_template) {
+                Write-Host "The .env.template has been updated."
+                Write-Host "Please update your .env file to include any new variables."
+            }
         }
     }
+    else {
+        Write-Host "Auto-GPT is already up to date."
+    }
 
+    Set-Location ..
+}
+else {
     # Clone the Git repository
-    $git_url = "https://github.com/Torantulino/Auto-GPT.git"
-    git clone $git_url cloned_repo
-
-    # Modify the .env file in the cloned repository
-    $env_template = Get-Content "cloned_repo/.env.template" -Raw
-    $env_template = $env_template.Replace("your-openai-api-key", $OPENAI_API_KEY)
-    $env_template = $env_template.Replace("your-pinecone-api-key", $PINECONE_API_KEY)
-    $env_template = $env_template.Replace("your-pinecone-region", $PINECONE_API_ENV)
-    Set-Content -Path "cloned_repo/.env" -Value $env_template
-
-    # Create start_auto_gpt.ps1 script in the cloned repository
-    $start_auto_gpt_script = @"
-`$continuous = Read-Host 'Do you want to run AutoGPT in continuous mode? (y/n) (default n): '
-`$gpt_version = Read-Host 'Which GPT version do you want to use? Enter 3 for GPT-3, any other key for GPT-4 (default GPT-4): '
-if (`$gpt_version -eq '3') {
-    `$gpt_flag = '--gpt3only'
-} else {
-    `$gpt_flag = ''
-}
-if (`$continuous -eq 'y' -or `$continuous -eq 'Y') {
-    python scripts/main.py --continuous `$gpt_flag
-} else {
-    python scripts/main.py `$gpt_flag
-}
-"@
-    Set-Content -Path "cloned_repo/start_auto_gpt.ps1" -Value $start_auto_gpt_script
-
+    $git_url = "https://github.com/Significant-Gravitas/Auto-GPT.git"
+    git clone -b stable $git_url Auto-GPT
+    
     # Add to .gitignore
-    Add-Content "cloned_repo/.gitignore" ".env"
-    Add-Content "cloned_repo/.gitignore" "start_auto_gpt.ps1"
-
-    # Create Dockerfile
-    $dockerfile_content = @"
-FROM python:3.8
-
-RUN apt-get update && \
-    apt-get install -y libgirepository1.0-dev
-
-WORKDIR /app
-
-COPY ./cloned_repo /app
-
-RUN pip install --no-cache-dir -r requirements.txt && \
-    pip install --no-cache-dir PyGObject && \
-    chmod +x start_auto_gpt.ps1
-
-CMD ["powershell", "-c", "git config pull.rebase false && ./start_auto_gpt.ps1"]
-"@
-    Set-Content -Path "Dockerfile" -Value $dockerfile_content
-
-    # Create .dockerignore
-    Set-Content -Path ".dockerignore" "__pycache__"
-
-    # Build Docker image
-    docker build -t auto-gpt .
-
-    # Remove the cloned git repository from the local machine
-    Remove-Item -Recurse -Force "cloned_repo"
-
-    # Run Docker container
-    docker run -it --name auto-gpt-container -v "${workspace_path}:/app/auto_gpt_workspace" auto-gpt
-
-} else {
-    docker start -ai auto-gpt-container
+    Add-Content -Path ./Auto-GPT/.gitignore -Value ".env"
 }
+
+# Modify the .env file in the cloned repository
+# Check if .env file exists
+if (Test-Path "./Auto-GPT/.env") {
+    Write-Host ".env file exists. Loading your current configuration."
+}
+else {
+    Write-Host "Creating .env file from template..."
+    Copy-Item ./Auto-GPT/.env.template ./Auto-GPT/.env
+
+    $openai_api_key = Read-Host "Enter your OpenAI API key: "
+    (Get-Content ./Auto-GPT/.env).Replace('your-openai-api-key', $openai_api_key) | Set-Content ./Auto-GPT/.env
+    Write-Host "OpenAI API key has been set in the .env file."
+}
+
+# Prompt user for workspace folder path
+$workspace_path = Read-Host "Enter the workspace folder [Enter for default: $($env:USERPROFILE)/auto_gpt_workspace]"
+if ([string]::IsNullOrEmpty($workspace_path)) {
+    $workspace_path = "$($env:USERPROFILE)/auto_gpt_workspace"
+}
+New-Item -ItemType Directory -Force -Path $workspace_path
+
+# Build Docker image
+Write-Host "Building Docker image for Auto-GPT..."
+try {
+    docker build -t auto-gpt ./Auto-GPT
+} catch {
+    Write-Host "An error occurred while building the Docker image. Please check the error message and try again."
+    exit
+}
+
+# Prompt user for continuous mode and GPT-3 only mode
+$continuous_mode = Read-Host "Do you want to run in continuous mode? (y/n): "
+if ($continuous_mode -eq "y") {
+    $continuous_flag = "--continuous"
+}
+else {
+    $continuous_flag = ""
+}
+
+$gpt3_only_mode = Read-Host "Do you want to use GPT-3 only? (y/n): "
+if ($gpt3_only_mode -eq "y") {
+    $gpt3_only_flag = "--gpt3only"
+}
+else {
+    $gpt3_only_flag = ""
+}
+
+# Run Docker container
+Write-Host "Starting Auto-GPT Docker container..."
+docker run -it --env-file=./Auto-GPT/.env -v "${workspace_path}:/home/appuser/auto_gpt_workspace" auto-gpt $gpt3_only_flag $continuous_flag
