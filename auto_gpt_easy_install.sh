59fef14056d75e60e5f7d2e958e03512f114ed3c
Ruggero <ruggero.galloni@gmail.com>
2023-04-22 19:25:59 +0200

    Working with official Dockerfile + auto-update
    The following updates have been made:
    - Now it works with official Docker file to future proof implementations
    - It will keeps your Auto-GPT updated with the last release (if you ask for)
    - Improved user interactions and information requests throughout the script

--- auto_gpt_easy_install.sh
+++ auto_gpt_easy_install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+echo "Welcome to the Auto-GPT setup script! This script will guide you through the installation and configuration process."
+
 # Check if Docker is installed, otherwise install it
 if ! command -v docker >/dev/null; then
-    echo "Docker not found. Installing Docker..."
+    echo "Docker not found. Installing Docker now..."
     if command -v apt-get >/dev/null; then
         curl -fsSL https://get.docker.com -o get-docker.sh
         sudo sh get-docker.sh
@@ -11,114 +13,97 @@ if ! command -v docker >/dev/null; then
     elif command -v brew >/dev/null; then
         brew install docker
     else
-        echo "Error: Unable to find a suitable package manager to install Docker"
+        echo "Error: Unable to find a suitable package manager to install Docker."
         exit 1
     fi
 fi
 
-# Check if the Docker container exists
-container_exists=$(docker ps -a --filter "name=auto-gpt-container" --format "{{.Names}}")
-
-# Run Docker container if it doesn't exist, or start the existing container
-if [ -z "$container_exists" ]; then
-
-    # Remove the cloned git repository from the local machine
-    rm -rf cloned_repo
-
-    # Prompt user for API keys
-    read -p "Enter your OpenAI API Key: " OPENAI_API_KEY
-    #read -p "Enter your ElevenLabs API Key (leave empty if not using Speech Mode): " ELEVEN_LABS_API_KEY
-
-    read -p "Enter your Pinecone API Key (long term memory: get for free at https://pinecone.io): " PINECONE_API_KEY
-
-    read -p "Enter your Pinecone API Env (long term memory: get for free at https://pinecone.io): " PINECONE_API_ENV
-
-    # Prompt user for workspace folder path
-    read -p "Enter the path of the workspace folder [press Enter for the default path: $HOME/auto_gpt_workspace]": workspace_path
-    if [ -z "$workspace_path" ]; then
-        workspace_path="$HOME/auto_gpt_workspace"
+# Check if git is installed, otherwise install it
+if ! command -v git >/dev/null; then
+    echo "Git not found. Installing Git now..."
+    if command -v apt-get >/dev/null; then
+        sudo apt-get update && sudo apt-get install -y git
+    elif command -v brew >/dev/null; then
+        brew install git
+    else
+        echo "Error: Unable to find a suitable package manager to install Git."
+        exit 1
     fi
-    mkdir -p "$workspace_path"
+fi
 
-    # Check if git is installed, otherwise install it
-    if ! command -v git >/dev/null; then
-        if command -v apt-get >/dev/null; then
-            sudo apt-get update && sudo apt-get install -y git
-        elif command -v brew >/dev/null; then
-            brew install git
-        else
-            echo "Error: Unable to find a suitable package manager to install git"
-            exit 1
+# Check if Auto-GPT folder exists and if it has a .git directory
+if [ -d "Auto-GPT" ] && [ -d "Auto-GPT/.git" ]; then
+    cd Auto-GPT
+    # Fetch the latest changes from the remote repository
+    git fetch
+
+    # Check if there are updates available
+    if git status -uno | grep -q "Your branch is behind"; then
+        read -p "Updates are available. Would you like to update Auto-GPT to the latest version? (y/n): " update_choice
+        if [ "$update_choice" == "y" ]; then
+            pre_pull_env_template=$(cat .env.template)
+            git pull
+            post_pull_env_template=$(cat .env.template)
+            
+            if [ "$pre_pull_env_template" != "$post_pull_env_template" ]; then
+                echo "The .env.template has been updated. Here are the differences:"
+                diff -u <(echo "$pre_pull_env_template") <(echo "$post_pull_env_template")
+                echo "Please update your .env file to include any new variables."
+            fi
         fi
+    else
+        echo "Auto-GPT is already up to date."
     fi
 
+    cd ..
+else
     # Clone the Git repository
-    git_url="https://github.com/Torantulino/Auto-GPT.git"
-    git clone $git_url cloned_repo
-
-    # Modify the .env file in the cloned repository
-    env_template=$(cat cloned_repo/.env.template)
-    env_template=${env_template//your-openai-api-key/$OPENAI_API_KEY}
-    env_template=${env_template//your-pinecone-api-key/$PINECONE_API_KEY}
-    env_template=${env_template//your-pinecone-region/$PINECONE_API_ENV}
-    #env_template=${env_template//your-elevenlabs-api-key/$ELEVEN_LABS_API_KEY}
-    echo "$env_template" > cloned_repo/.env
-
-    # Create start_auto_gpt.sh script in the cloned repository
-    cat > cloned_repo/start_auto_gpt.sh <<EOL
-#!/bin/bash
-
-read -p "Do you want to run AutoGPT in continuous mode? (y/n) (default n): " continuous
-
-read -p "Which GPT version do you want to use? Enter 3 for GPT-3, any other key for GPT-4 (default GPT-4): " gpt_version
-
-if [ "$gpt_version" == "3" ]; then
-    gpt_flag="--gpt3only"
-else
-    gpt_flag=""
-fi
-
-if [ "$continuous" == "y" ] || [ "$continuous" == "Y" ]; then
-    python scripts/main.py --continuous $gpt_flag
-else
-    python scripts/main.py $gpt_flag
-fi
-EOL
-
+    git_url="https://github.com/Significant-Gravitas/Auto-GPT.git"
+    git clone -b stable $git_url Auto-GPT
+    
     # Add to .gitignore
-    echo ".env" >> cloned_repo/.gitignore
-    echo "start_auto_gpt.sh" >> cloned_repo/.gitignore
+    echo ".env" >> ./Auto-GPT/.gitignore
+fi
 
-    # Create Dockerfile
-    cat > Dockerfile <<EOL
-FROM python:3.8
+# Modify the .env file in the cloned repository
+# Check if .env file exists
+if [ -f "./Auto-GPT/.env" ]; then
+    echo ".env file exists. Loading your current configuration."
+else
+    echo "Creating .env file from template..."
+    cp ./Auto-GPT/.env.template ./Auto-GPT/.env
 
-RUN apt-get update && \
-    apt-get install -y libgirepository1.0-dev
+    read -p "Enter your OpenAI API key: " openai_api_key
+    awk -v api_key="$openai_api_key" '{gsub(/your-openai-api-key/, api_key); print}' ./Auto-GPT/.env > ./Auto-GPT/.env.tmp && mv ./Auto-GPT/.env.tmp ./Auto-GPT/.env
+    echo "OpenAI API key has been set in the .env file."
+fi
 
-WORKDIR /app
-
-COPY ./cloned_repo /app
-
-RUN pip install --no-cache-dir -r requirements.txt && \
-    pip install --no-cache-dir PyGObject && \
-    chmod +x start_auto_gpt.sh
-
-CMD ["bash", "-c", "git config pull.rebase false && ./start_auto_gpt.sh"]
-EOL
-
-# Create .dockerignore
-echo "__pycache__" > .dockerignore
+# Prompt user for workspace folder path
+read -p "Enter the workspace folder [Enter for default: $HOME/auto_gpt_workspace]": workspace_path
+if [ -z "$workspace_path" ]; then
+    workspace_path="$HOME/auto_gpt_workspace"
+fi
+mkdir -p "$workspace_path"
 
 # Build Docker image
-docker build -t auto-gpt .
+echo "Building Docker image for Auto-GPT..."
+docker build -t auto-gpt ./Auto-GPT
 
-# Remove the cloned git repository from the local machine
-rm -rf cloned_repo
+# Prompt user for continuous mode and GPT-3 only mode
+read -p "Do you want to run in continuous mode? (y/n): " continuous_mode
+if [ "$continuous_mode" == "y" ]; then
+    continuous_flag="--continuous"
+else
+    continuous_flag=""
+fi
+
+read -p "Do you want to use GPT-3 only? (y/n): " gpt3_only_mode
+if [ "$gpt3_only_mode" == "y" ]; then
+    gpt3_only_flag="--gpt3only"
+else
+    gpt3_only_flag=""
+fi
 
 # Run Docker container
-docker run -it --name auto-gpt-container -v "${workspace_path}:/app/auto_gpt_workspace" auto-gpt
-
-else
-    docker start -ai auto-gpt-container
-fi
+echo "Starting Auto-GPT Docker container..."
+docker run -it --env-file=./Auto-GPT/.env -v "${workspace_path}:/home/appuser/auto_gpt_workspace" auto-gpt $gpt3_only_flag $continuous_flag
